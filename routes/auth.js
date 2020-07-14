d312a441af11092e712696b518a20066f3d153eb
skingz77 <haisem.jemal@gmail.com>
2020-07-14 13:17:37 -0400

    added some comments

--- routes/auth.js
+++ routes/auth.js
@@ -66,7 +66,7 @@ router.post('/register', async (req, res) => {
  * It will login the user and send them a json web token
  * 
  * How does it work
- * as
+ * asd
  */
 
 router.post('/login', async (req, res) => {
